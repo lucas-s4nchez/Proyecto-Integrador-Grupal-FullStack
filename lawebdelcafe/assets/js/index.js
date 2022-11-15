2c5905975481142701a6cd2764659d24a3dce405
Lucas <lucassanchezdl@gmail.com>
2022-11-14 21:41:28 -0300

    logo

---
+++ lawebdelcafe/assets/js/index.js
@@ -0,0 +1,8 @@
+const inputName = document.getElementById("name");
+const inputLastname = document.getElementById("lastname");
+const inputMail = document.getElementById("mail");
+const inputText = document.getElementById("textarea");
+
+const validateForm = (e) => {
+  e.preventDefault();
+};
